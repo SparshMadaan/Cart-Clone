25b03c488b7a6f6b745b2ea9d7ea538883be7a1c
Sparsh Madaan <sparshmadaan9600@gmail.com>
2024-09-27 04:48:09 +0530

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import "./index.css";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Home />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/CartDetails.jsx
@@ -0,0 +1,10 @@
+import "./cartstyle.css";
+const CartDetails = () => {
+  return (
+    <>
+      <h1>This is cartDetails</h1>
+    </>
+  );
+};
+
+export default CartDetails;
